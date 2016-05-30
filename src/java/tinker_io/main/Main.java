de3ec386f098eedd3bc8b7ad411b740ec7566e96
tokomikea <tokomikeaemail@gmail.com>
2016-05-30 20:49:57 +0800

    fim is valid smeltery block

--- src/java/tinker_io/main/Main.java
+++ src/java/tinker_io/main/Main.java
@@ -1,5 +1,6 @@
 package tinker_io.main;
 
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 import slimeknights.tconstruct.tools.TinkerTools;
 import tinker_io.packet.PacketDispatcher;
 import tinker_io.plugins.waila.MainWaila;
@@ -12,6 +13,10 @@ import tinker_io.registry.ItemRenderRegister;
 import tinker_io.registry.OreCrusherBanLiatRegistry;
 import tinker_io.registry.SmelteryRecipeRegistry;
 import tinker_io.registry.RecipeRegistry;
+
+import com.google.common.collect.ImmutableSet;
+
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
@@ -115,5 +120,13 @@ public class Main
 		  proxy.postInit(event);
 		  proxy.registerNetworkStuff();
 		  SmelteryRecipeRegistry.registerMeltingCasting();
+		  
+		  ImmutableSet.Builder<Block> builder = ImmutableSet.builder();
+          for(Block block : TinkerSmeltery.validSmelteryBlocks)
+          {
+              builder.add(block);
+          }
+          builder.add(BlockRegistry.fuelInputMachine);
+          TinkerSmeltery.validSmelteryBlocks = builder.build();
 	  }
 }
